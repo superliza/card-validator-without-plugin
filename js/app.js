103056971a957324f48d5925625a76fe140edd32
superliza <eli_darko@hotmail.com>
2018-02-26 09:51:02 -0600

    se agrega función de validar nombre

--- js/app.js
+++ js/app.js
@@ -1,27 +1,34 @@
-
 // jalar formulario
 // convertirlo a Array.apply
 const form = document.querySelector("form");
-// console.log(form);
+// se traen elementos padres de los input.
+let divCard = document.getElementById("div-card");
+let divName = document.getElementById("div-name");
+let cvvDiv = document.getElementById("div-cvv");
+
+// el elementos "form" se convierte a array.
 const colectionToArray = Array.from(form);
-// console.log(colectionToArray);
+// se le da el evento de submit a form y se crea una función anónima que recibirá las demás funciones.
 form.addEventListener("submit", e => {
     e.preventDefault();
-    validCardCredentials(form);
+    validCardName(form);
+    validCard(form);
     cardVerificationValue(form);
 });
 
-function validCardCredentials(form) {
-    let numberCard = colectionToArray[0].value;
+// se declara la función de la tarjeta de crédito y se le pasa el parámetro de form.
+function validCard(form) {
+    let numberCard = colectionToArray[0].value; 
     if (numberCard.charCodeAt() >= 48 && numberCard.charCodeAt() <= 57) {
         let numberArray = Array.from(numberCard);
         if (numberArray.length === 16) {
             let reverseArray = numberArray.reverse();
-            const hola = reverseArray.map((element, index) => {
+            const luhnAlgorithm = reverseArray.map((element, index) => {
                 let unmodifiedElements = parseInt(element);
                 if (index % 2 == 0) { 
                     return unmodifiedElements;
                 }
+
                 if (index % 2 != 0) {
                     let multiplyElements = parseInt(element * 2);
                     // console.log(multiplyElements);
@@ -32,36 +39,61 @@ function validCardCredentials(form) {
                     return multiplyElements
                 }    
             }).reduce((a, b) => a + b, 0);
-            if (hola % 10 == 0) {
-                console.log("tarjeta válida");    
+            if (luhnAlgorithm % 10 == 0) {
+                divCard.classList.add("has-success");    
             } else {
-                console.log("tarjeta inválida");    
+                divCard.classList.add("has-error");    
             } 
         } else {
-            console.log("inválido");            
+            divCard.classList.add("has-error");            
         }
-    } else {
-        console.log(false);        
+    } else {  
+        divCard.classList.add("has-error");     
     }
 }
 
 
-
+// se declara la función que valida los tres números.
 const cardVerificationValue = form => {
-    const date = Array.from(colectionToArray[2].value);
-    if (date.length === 3) {
-        date.forEach(element => {
+    const cvv = Array.from(colectionToArray[2].value);
+    if (cvv.length === 3) {
+        cvv.forEach(element => {
             if (element.charCodeAt() >= 48 && element.charCodeAt() <= 57) {
-                    console.log(element);   
+                cvvDiv.classList.add("has-success");   
                     return element;    
             } else {
-                console.log(false); 
+                cvvDiv.classList.add("has-error"); 
                 return false;           
             }
         })      
     } else {
-        console.log("adiós");       
+        cvvDiv.classList.add("has-error");       
     }
 }
 
+// se declara la función que valida el nombre.
+const validCardName = form => {
+    let name = colectionToArray[3].value.trim();
+    let mySplit = name.split(" ");
+        // console.log(mySplit);
 
+    if (mySplit.length > 1) {
+        // console.log("hola");
+        const validateName = mySplit.forEach(element => {
+            if (element.length > 2 && element.length < 31) {
+                let upperCaseLetters = element.charCodeAt() >= 65 && element.charCodeAt() <= 90;
+                let lowerCaseLetters = element.charCodeAt() >= 97 && element.charCodeAt() <= 122;
+                let space = element.charCodeAt() === 32;
+                if (upperCaseLetters || lowerCaseLetters || space) {
+                    console.log(element);        
+                } else {
+                    divName.classList.add("has-error");    
+                }
+            } else {
+                divName.classList.add("has-error");
+            } 
+       });
+   } else {
+    divName.classList.add("has-error");    
+   }
+}
