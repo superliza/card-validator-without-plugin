2ae29c82338e1c44a6604288a4a7ee6749659aa4
superliza <eli_darko@hotmail.com>
2018-02-26 18:25:10 -0600

    se arregla problema del nombre, ya no acepta carecteres extraños, pero sólo se muestra en consola

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ let cvvDiv = document.getElementById("div-cvv");
 
 // el elementos "form" se convierte a array.
 const colectionToArray = Array.from(form);
+
 // se le da el evento de submit a form y se crea una función anónima que recibirá las demás funciones.
 form.addEventListener("submit", e => {
     e.preventDefault();
@@ -55,6 +56,7 @@ function validCard(form) {
 
 // se declara la función que valida los tres números.
 const cardVerificationValue = form => {
+    // se convierte 
     const cvv = Array.from(colectionToArray[2].value);
     if (cvv.length === 3) {
         cvv.forEach(element => {
@@ -73,27 +75,50 @@ const cardVerificationValue = form => {
 
 // se declara la función que valida el nombre.
 const validCardName = form => {
-    let name = colectionToArray[3].value.trim();
-    let mySplit = name.split(" ");
-        // console.log(mySplit);
-
-    if (mySplit.length > 1) {
-        // console.log("hola");
-        const validateName = mySplit.forEach(element => {
-            if (element.length > 2 && element.length < 31) {
-                let upperCaseLetters = element.charCodeAt() >= 65 && element.charCodeAt() <= 90;
-                let lowerCaseLetters = element.charCodeAt() >= 97 && element.charCodeAt() <= 122;
-                let space = element.charCodeAt() === 32;
-                if (upperCaseLetters || lowerCaseLetters || space) {
-                    console.log(element);        
-                } else {
-                    divName.classList.add("has-error");    
-                }
+    let name = Array.from(colectionToArray[3].value.trim().toLowerCase());
+    // let words = name.join("");
+    // let wordsArray = words.split(" ");
+    
+    // const logic = wordsArray.forEach((element, index, array) => {
+    //     if (element.charCodeAt() >= 97 && element.charCodeAt() <= 122 || element.charCodeAt() === 32) {
+    //         console.log(element);  
+    //     } else {
+    //         console.log(false);
+            
+    //     }
+        
+    // })
+    if (name.length >= 3 && name.length <= 30) {
+        const validateName = name.map(element => {
+            if (element.charCodeAt() >= 97 && element.charCodeAt() <= 122 || element.charCodeAt() === 32) {
+                // console.log(element); 
+                return element  
             } else {
-                divName.classList.add("has-error");
-            } 
-       });
-   } else {
-    divName.classList.add("has-error");    
-   }
+                console.log(("no está dentro del rango"));
+            }
+            
+        })
+        const ash = validateName.join("").split(" ");
+        if (ash.length >= 2) {
+            console.log(ash);
+            
+        } else {
+            console.log("escribe mínimo dos palabras");  
+        }
+        
+    } else {
+        console.log("Escribe mínimo 2 caracteres");
+        
+    }   
 }
+
+
+// const validCardName = form => {
+//     let inputValue = Array.from(colectionToArray[3].value.trim().toLowerCase());
+//     inputValue = (inputValue.length >= 3 && inputValue.length <= 30) ? console.log(inputValue) : console.log("no es mayor");
+    
+    
+// } 
+// primero hago el conteo de las letras y sin son mayores o igual a dos y menores o igual a 31, entonces hace el recorrido.
+// Y una vez hecho el recorrido (tal vez con el forEach), se compara el valor del input con el dófigo ascii y tiene que ser 
+// mayor o igual a 97 y menor o igual a 12 y si esto es verdadero, se hace el split y su longitud tiene que ser mayor o igaul a 2.
